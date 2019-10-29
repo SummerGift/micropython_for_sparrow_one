86c03619cc20cf5f463e0ac3297c0495aaf0a34a
SummerGift <SummerGift@qq.com>
2019-10-29 17:33:53 +0800

    [add] wifi connect func for rtthread module

--- firmware/packages/micropython-v1.10.1/port/modules/modrtthread.c
+++ firmware/packages/micropython-v1.10.1/port/modules/modrtthread.c
@@ -41,6 +41,12 @@ rt_bool_t rt_is_preempt_thread(void) {
     }
 }
 
+STATIC void error_check(bool status, const char *msg) {
+    if (!status) {
+        nlr_raise(mp_obj_new_exception_msg(&mp_type_OSError, msg));
+    }
+}
+
 STATIC mp_obj_t mod_is_preempt_thread(void) {
     return mp_obj_new_bool(rt_is_preempt_thread());
 }
@@ -98,6 +104,64 @@ STATIC mp_obj_t mod_http_ota(mp_obj_t http_url) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_http_ota_obj, mod_http_ota);
 
+STATIC mp_obj_t wlan_connect(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    #include "wlan_dev.h"
+    #include "wlan_cmd.h"
+    #include "drv_wlan.h"
+    static int network_mode      = WIFI_STATION;
+
+    enum { ARG_ssid, ARG_password, ARG_bssid };
+    static const mp_arg_t allowed_args[] = {
+        { MP_QSTR_, MP_ARG_OBJ, {.u_obj = mp_const_none} },
+        { MP_QSTR_, MP_ARG_OBJ, {.u_obj = mp_const_none} },
+        { MP_QSTR_bssid, MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
+    };
+
+    // parse args
+    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all(n_args - 1, pos_args + 1, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+
+    const char *ssid = RT_NULL;
+    const char *key = RT_NULL;
+    size_t len;
+    const char *p;
+
+    // set parameters based on given args
+    if (args[ARG_ssid].u_obj != mp_const_none) {
+        p = mp_obj_str_get_data(args[ARG_ssid].u_obj, &len);
+        ssid = p;
+    }
+
+    if (args[ARG_password].u_obj != mp_const_none) {
+        p = mp_obj_str_get_data(args[ARG_password].u_obj, &len);
+        key = p;
+    }
+
+    /* get wlan device */
+    struct rt_wlan_device *wlan;
+    wlan = (struct rt_wlan_device *)rt_device_find("w0");
+    if (!wlan)
+    {
+        rt_kprintf("no wlan:%s device\n", "w0");
+        return 0;
+    }
+
+    mp_printf(&mp_plat_print, "begin to connect WIFI\n");
+    mp_printf(&mp_plat_print, "SSID: %s\n", ssid);
+    mp_printf(&mp_plat_print, "key: %s\n", key);
+
+    rt_wlan_init(wlan, WIFI_STATION);
+    network_mode = WIFI_STATION;
+
+    rt_wlan_info_init(&info, WIFI_STATION, SECURITY_WPA2_MIXED_PSK, ssid);
+    error_check(rt_wlan_connect(wlan, &info, key) == RT_EOK, "Cannot connect to AP");
+    rt_wlan_info_deinit(&info);
+    mp_printf(&mp_plat_print, "Connect wifi successful.");
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(mod_wlan_connect_obj, 3, wlan_connect);
+
 STATIC const mp_rom_map_elem_t mp_module_rtthread_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_rtthread) },
     { MP_ROM_QSTR(MP_QSTR_is_preempt_thread), MP_ROM_PTR(&mod_is_preempt_thread_obj) },
@@ -105,6 +169,7 @@ STATIC const mp_rom_map_elem_t mp_module_rtthread_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_stacks_analyze), MP_ROM_PTR(&mod_stacks_analyze_obj) },
     { MP_ROM_QSTR(MP_QSTR_list_device), MP_ROM_PTR(&mod_list_device_obj) },
     { MP_ROM_QSTR(MP_QSTR_http_ota), MP_ROM_PTR(&mod_http_ota_obj) },
+    { MP_ROM_QSTR(MP_QSTR_wifi_join), MP_ROM_PTR(&mod_wlan_connect_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(mp_module_rtthread_globals, mp_module_rtthread_globals_table);
